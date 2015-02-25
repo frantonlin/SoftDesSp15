ce2749b0a20591f0359eb69f3b1b175d5a23f7b2
Franton Lin <franton.lin@students.olin.edu>
2015-02-25 18:08:48 -0500

    Semi-working plotting

--- data_mining/facebook_links.py
+++ data_mining/facebook_links.py
@@ -20,17 +20,7 @@ import cPickle
 from pattern.web import Facebook, NEWS, COMMENTS, LIKES, FRIENDS
 import csv
 import matplotlib.pyplot as plt
-
-def save_to_csv(data, filename):
-		""" Writes given data in list of tuples to a .csv file with the given filename
-
-			data: the list of tuples to write to a .csv
-			filename: the name of the .csv to write
-		"""
-			writer = csv.writer(f)
-			writer.writerow(['URL', 'num_posts', 'num_likes'])	
-			for row in data:
-				writer.writerow(row)
+import numpy as np
 
 # removed my key
 def fetch_data(data="facebook.pickle", friends_count=1000, news_count=100, key='REMOVED MY KEY'):
@@ -88,37 +78,39 @@ def process_data(data='facebook.pickle'):
 	# Process the data
 	short_counts = {"num_posts":link_counts["num_posts"], "num_links":link_counts["num_links"]}
 	for url in link_counts:
-		#print url
+		#	print url
 		if url != "num_posts" and url != "num_links" and re.match('http',url):	# not a post counter and is a valid URL
-			if "facebook" in url:		# most cases of Facebook related URLs
-				if "video.php" in url:	# shared video
+			if "facebook" in url:			# most cases of Facebook related URLs
+				if "video.php" in url:		# shared video
 					short_url = re.search('//[^\t\n\r\f\v]*video.php', url).group()[2:]
-				elif "/photos/" in url:	# shared photo
+				elif "/photos/" in url:		# shared photo
 					short_url = re.search('//[^\t\n\r\f\v]*photos', url).group()[6:]
-				elif "posts" in url:	# link to post
+				elif "posts" in url:		# link to post
 					short_url = re.search('//[^\t\n\r\f\v]*posts', url).group()[2:]
-				elif re.match('https*://www.facebook.com/[^\t\n\r\f\v/]*',url): # profile
+				elif re.match('https*://www.facebook.com/[^\t\n\r\f\v/]*',url):	# profile
 					short_url = re.search('//[^\t\n\r\f\v]*', url).group()[2:]
-				else:					# other mystery case
+				else:						# other mystery case
 					short_url = re.search('//[^\t\n\r\f\v/]*/', url).group()[2:-1]
 			else:
-				if "fbcdn-" in url:		# one instance of Facebook photos
+				if "fbcdn-" in url:			# one instance of Facebook photos
 					list_url = list(re.search('//[^\t\n\r\f\v/]*/', url).group()[2:-1])
 					list_url[14] = '*'
 					list_url[16] = '*'
 					short_url = "".join(list_url)
-				elif "youtu" in url:	# youtube
+				elif "youtu" in url:		# youtube
 					short_url = "youtube.com"
-				elif "imgur" in url:	# imgur
+				elif "imgur" in url:		# imgur
 					short_url = "imgur.com"
-				elif "//t." in url:
+				elif "//t." in url:			# twitter
 					short_url = "twitter.com"
-				elif "//m." in url:		# mobile links
+				elif re.search('flic.?kr', url):	# flickr nyti.ms vs mytimes.com, es.pn vs espn.go.com
+					short_url = "flickr.com"
+				elif "//m." in url:			# mobile links
 					short_url = re.search('//[^\t\n\r\f\v/]*/', url).group()[4:-1]
 				elif url[-1] != "/" and "/" not in re.search('//[^\t\n\r\f\v]*', url).group()[2:-1]:	# url without '/' at end
 					short_url = re.search('//[^\t\n\r\f\v/]*', url).group()[2:]
-				else:					# every other case
-					if "//www2" in url:	# remove www.
+				else:						# every other case
+					if "//www2" in url:		# remove www.
 						short_url = re.search('//[^\t\n\r\f\v/]*/', url).group()[7:-1]
 					elif "//www" in url:
 						short_url = re.search('//[^\t\n\r\f\v/]*/', url).group()[6:-1]
@@ -132,12 +124,32 @@ def process_data(data='facebook.pickle'):
 			else:
 				short_counts[short_url] = link_counts[url]
 
-	# Sort from highest to lowest cumulative likes
 	sort_list = [(k, v[0], v[1]) for k, v in short_counts.iteritems() if k != "num_posts" and k != "num_links"]
+	num_entries = 50
+
+	# Sort from highest to lowest number of posts
+	sort_list.sort(key=lambda tup: tup[1], reverse = True)
+	# Plot number of posts
+	plot_bar_graph([tup[0] for tup in sort_list[:num_entries]], "URL",
+		[tup[1] for tup in sort_list[:num_entries]], "Number of Posts",
+		num_entries, "URLs with Highest Number of Posts")	
+
+	# Sort from highest to lowest cumulative likes
 	sort_list.sort(key=lambda tup: tup[2], reverse = True)
+	# Plot cumulative likes
+	plot_bar_graph([tup[0] for tup in sort_list[:num_entries]], "URL",
+		[tup[2] for tup in sort_list[:num_entries]], "Cumulative Likes",
+		num_entries, "URLs with Highest Cumulative Likes")
+
+	# Sort from highest to lowest average likes per post
+	sort_list.sort(key=lambda tup: float(tup[2])/tup[1], reverse = True)
+	# Plot average likes per post
+	plot_bar_graph([tup[0] for tup in sort_list[:num_entries]], "URL",
+		[float(tup[2])/tup[1] for tup in sort_list[:num_entries]], "Average Likes per Post",
+		num_entries, "URLs with Highest Average Likes per Post")
 
 	# Save the results
-	save_to_csv(sort_list, data[:-6] + "csv")
+	#save_to_csv(sort_list, data[:-6] + "csv")
 
 	# Print the results
 	#for item in sort_list:
@@ -146,7 +158,47 @@ def process_data(data='facebook.pickle'):
 	print "num_posts: " + str(short_counts["num_posts"])
 	print "num_links: " + str(short_counts["num_links"])
 
+def plot_bar_graph(x_data, x_label, y_data, y_label, num_entries, title):
+	fig = plt.figure(figsize=(20, 10))
+	ax = plt.subplot(111)
+
+
+	index = np.arange(num_entries)
+	bar_width = 0.7
+
+	opacity = 1
+	error_config = {'ecolor': '0.3'}
+
+	#print data[:10][2]
+
+	rects =ax.bar(index, y_data, bar_width,
+	                 alpha=opacity,
+	                 color='b',
+	                 error_kw=error_config)
+
+	plt.xlabel(x_label)
+	plt.ylabel(y_label)
+	plt.title(title)
+	#ax.set_xticklabels([tup[x_id] for tup in data[:num_entries]], rotation=90, rotation_mode="anchor")
+	plt.tick_params(axis='both', which='major', labelsize=12)
+	plt.xticks(index + bar_width / 2.0, x_data, rotation=90)
+
+	plt.tight_layout()
+	plt.show()
+
+
+def save_to_csv(data, filename):
+	""" Writes given data in list of tuples to a .csv file with the given filename
+			data: the list of tuples to write to a .csv
+		filename: the name of the .csv to write
+	"""
+	with open(filename, "w") as f:
+		writer = csv.writer(f)
+		writer.writerow(['URL', 'num_posts', 'num_likes'])	
+		for row in data:
+			writer.writerow(row)
+
 #fetch_data()
-process_data('facebook_10_10.pickle')
+process_data('facebook_1000_250.pickle')
 
 #print "Text:\t" + (news.text).encode('utf-8')
